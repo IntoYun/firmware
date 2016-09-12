86a0323fda6efd1e287d3a83852449bc93a25eda
baoxianzhang <baoxianzhit@gmail.com>
2016-09-12 11:59:52 +0800

    修改pwm个数为7个，增加eeprom接口并测试通过。

--- hal/src/nut/flash_map.h
+++ hal/src/nut/flash_map.h
@@ -77,5 +77,3 @@
 #define MAC_SEC_NUM                      1    //mac 地址暂存扇区个数
 #define FLASH_MAC_START_ADDR           MAC_SEC_START * SPI_FLASH_SEC_SIZE      //boot参数起始地址
 #endif /*FLASH_MAP_H_*/
-
-
