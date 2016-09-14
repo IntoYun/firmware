a549f443baf25cfc047ed5caf2d316e37fd36004
chy <chy-hitsz@chydeMacBook-Pro.local>
2016-09-15 01:53:02 +0800

    update install-tool.sh

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -54,23 +54,23 @@ if [ $sysType = "Linux" ]; then
 
 elif [ $sysType = "Darwin" ]; then
     echo -e "\033[40;32m 1. install arm gnu toolchain\033[0m"
-    wget -nc https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2
-    tar -jxf gcc-arm-none-eabi-5_4-2016q2-20160622-mac.tar.bz2
-    mv gcc-arm-none-eabi-5_4-2016q2 ../gcc-arm-none-eabi
+    wget -nc -c -t 10 https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2
+    tar -jxvf  gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2
+    mv gcc-arm-none-eabi-4_9-2015q2 ../gcc-arm-none-eabi
     rm -rf gcc-arm-none-eabi-4_9-2015q2
 
     echo -e "\033[40;32m 2. install st-flash\033[0m"
-    wget -nc https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-osx.tar.gz
+    wget -nc -c -t 10 https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-osx.tar.gz
     tar -zxf stlink-1.2.0-osx.tar.gz
     mv stlink-1.2.0-osx ../stlink
 
     echo -e "\033[40;32m 3. install xtensa lx106 gnu toolchain\033[0m"
-    wget -nc http://arduino.esp8266.com/osx-xtensa-lx106-elf-gb404fb9-2.tar.gz
+    wget -nc -c -t 10 http://arduino.esp8266.com/osx-xtensa-lx106-elf-gb404fb9-2.tar.gz
     tar -zxf osx-xtensa-lx106-elf-gb404fb9-2.tar.gz
     mv xtensa-lx106-elf ../xtensa-lx106-elf
 
     echo -e "\033[40;32m 4. install esptool\033[0m"
-    wget -nc https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-osx.tar.gz
+    wget -nc -c -t 10 https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-osx.tar.gz
     tar -zxf esptool-0.4.11-osx.tar.gz
     mv esptool-0.4.11-osx ../esptool
     rm -rf esptool-0.4.11-osx
