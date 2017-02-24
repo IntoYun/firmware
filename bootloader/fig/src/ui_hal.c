da64fe4868c7a98b62e8fa4ccf4ff9850f2b9a99
chenkaiyao <robin@molmc.com>
2017-02-24 09:27:18 +0800

    1. 修正void HAL_UI_SysTick_Handler(void) 按键释放BUTTON_last_state处理

--- bootloader/fig/src/ui_hal.c
+++ bootloader/fig/src/ui_hal.c
@@ -191,7 +191,7 @@ void HAL_UI_SysTick_Handler(void)
         if(BUTTON_last_state)
         {
             TimingBUTTON = 0;
-            BUTTON_last_state = 1;
+            BUTTON_last_state = 0;
         }
     }
 }
