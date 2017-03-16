496647773a90d1d3de1008aafa1adbc4fb87da02
chenkaiyao <robin@molmc.com>
2017-03-17 01:26:22 +0800

    1. 解决neutron配置时，接收长数据出现错误。USB_USART_Available_Data 返回值uint8_t修改成int32_t 2. wiring_stream.cpp中timedRead函数 超时时间从100ms改成1000ms

--- hal/inc/variants/neutron/intorobot_config.h
+++ hal/inc/variants/neutron/intorobot_config.h
@@ -61,7 +61,7 @@
 #define configSETUP_ENABLE
 #define configSETUP_USBSERIAL_ENABLE
 //#define configSETUP_USARTSERIAL_ENABLE
-#define configSETUP_TCP_ENABLE
+//#define configSETUP_TCP_ENABLE
 #define configSETUP_UDP_ENABLE
 
 /* config network */
