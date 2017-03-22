2663fcd186f0c4b79fd671c95ced8d511465199c
chenkaiyao <robin@molmc.com>
2017-03-22 11:22:49 +0800

    1. 添加TODO

--- user/applications/smartLight-default/application.ino
+++ user/applications/smartLight-default/application.ino
@@ -2,6 +2,8 @@
  * 智能灯程序
  */
 
+//SerialDebugOutput debugOutput(115200, ALL_LEVEL);
+
 #define SMARTLIGHT_CMD_SWITCH    "channel/smartLight_0/cmd/switch"   //开关命令
 #define SMARTLIGHT_DATA_STATUS   "channel/smartLight_0/data/status"  //开关状态
 
