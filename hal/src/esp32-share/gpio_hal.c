b15bc13fc8388c8b8ded1f613820e09f4b288229
chenkaiyao <robin@molmc.com>
2017-03-28 21:40:38 +0800

    1. 修改fig gpio实现

--- hal/src/esp32-share/gpio_hal.c
+++ hal/src/esp32-share/gpio_hal.c
@@ -21,26 +21,9 @@ License along with this library; if not, see <http://www.gnu.org/licenses/>.
 #include "hw_config.h"
 #include "gpio_hal.h"
 #include "pinmap_impl.h"
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_attr.h"
-#include "esp_intr.h"
-#include "rom/ets_sys.h"
-#include "rom/gpio.h"
-#include "soc/gpio_reg.h"
-#include "soc/rtc_io_reg.h"
-#include "soc/gpio_struct.h"
-#include "soc/io_mux_reg.h"
+#include "esp32-hal-gpio.h"
 
 PinMode digitalPinModeSaved = PIN_MODE_NONE;
-
-#define ETS_GPIO_INUM       12
-
-typedef void (*voidFuncPtr)(void);
-static voidFuncPtr __pinInterruptHandlers[GPIO_PIN_COUNT] = {0,};
-
-#include "driver/rtc_io.h"
 
 /* Private function prototypes ----------------------------------------------*/
 inline bool is_valid_pin(pin_t pin) __attribute__((always_inline));
@@ -75,92 +58,46 @@ PinFunction HAL_Validate_Pin_Function(pin_t pin, PinFunction pinFunction)
  */
 void HAL_Pin_Mode(pin_t pin, PinMode setMode)
 {
+    uint8_t mode;
     EESP32_Pin_Info* PIN_MAP = HAL_Pin_Map();
     pin_t gpio_pin = PIN_MAP[pin].gpio_pin;
 
-    uint32_t rtc_reg = rtc_gpio_desc[gpio_pin].reg;
-    if(setMode == AN_INPUT) {
-        PIN_MAP[pin].pin_mode = AN_INPUT;
-        if(!rtc_reg) {
-            return;//not rtc pin
-        }
-        //lock rtc
-        uint32_t reg_val = ESP_REG(rtc_reg);
-        if(reg_val & rtc_gpio_desc[gpio_pin].mux){
-            return;//already in adc mode
-        }
-        reg_val &= ~(
-                (RTC_IO_TOUCH_PAD1_FUN_SEL_V << rtc_gpio_desc[gpio_pin].func)
-                |rtc_gpio_desc[gpio_pin].ie
-                |rtc_gpio_desc[gpio_pin].pullup
-                |rtc_gpio_desc[gpio_pin].pulldown);
-        ESP_REG(RTC_GPIO_ENABLE_W1TC_REG) = (1 << (rtc_gpio_desc[gpio_pin].rtc_num + RTC_GPIO_ENABLE_W1TC_S));
-        ESP_REG(rtc_reg) = reg_val | rtc_gpio_desc[gpio_pin].mux;
-        //unlock rtc
-        ESP_REG(DR_REG_IO_MUX_BASE + esp32_gpioMux[gpio_pin].reg) = ((uint32_t)2 << MCU_SEL_S) | ((uint32_t)2 << FUN_DRV_S) | FUN_IE;
-        return;
+    switch (setMode)
+    {
+        case OUTPUT:
+            PIN_MAP[pin].pin_mode = OUTPUT;
+            mode = ESP32_OUTPUT;
+            break;
+
+        case INPUT:
+            PIN_MAP[pin].pin_mode = INPUT;
+            mode = ESP32_INPUT;
+            break;
+
+        case INPUT_PULLUP:
+            PIN_MAP[pin].pin_mode = INPUT_PULLUP;
+            mode = ESP32_INPUT_PULLUP;
+            break;
+
+        case INPUT_PULLDOWN:
+            PIN_MAP[pin].pin_mode = INPUT_PULLDOWN;
+            mode = ESP32_INPUT_PULLDOWN;
+            break;
+
+        case AF_OUTPUT_DRAIN:   //Used internally for Alternate Function Output Drain(I2C etc)
+            PIN_MAP[pin].pin_mode = AF_OUTPUT_DRAIN;
+            mode = ESP32_OUTPUT_OPEN_DRAIN;
+            break;
+
+        case AN_INPUT:        //Used internally for ADC Input
+            PIN_MAP[pin].pin_mode = AN_INPUT;
+            mode = ESP32_ANALOG;
+            break;
+
+        default:
+            break;
     }
-
-    //RTC pins PULL settings
-    if(rtc_reg) {
-        //lock rtc
-        ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[gpio_pin].mux);
-        if(setMode & INPUT_PULLUP) {
-            ESP_REG(rtc_reg) = (ESP_REG(rtc_reg) | rtc_gpio_desc[gpio_pin].pullup) & ~(rtc_gpio_desc[gpio_pin].pulldown);
-        } else if(setMode & INPUT_PULLDOWN) {
-            ESP_REG(rtc_reg) = (ESP_REG(rtc_reg) | rtc_gpio_desc[gpio_pin].pulldown) & ~(rtc_gpio_desc[gpio_pin].pullup);
-        } else {
-            ESP_REG(rtc_reg) = ESP_REG(rtc_reg) & ~(rtc_gpio_desc[gpio_pin].pullup | rtc_gpio_desc[gpio_pin].pulldown);
-        }
-        //unlock rtc
-    }
-
-    uint32_t pinFunction = 0, pinControl = 0;
-
-    //lock gpio
-    if(setMode & INPUT) {
-        PIN_MAP[pin].pin_mode = INPUT;
-        if(gpio_pin < 32) {
-            GPIO.enable_w1tc = ((uint32_t)1 << gpio_pin);
-        } else {
-            GPIO.enable1_w1tc.val = ((uint32_t)1 << (gpio_pin - 32));
-        }
-    } else if(setMode & OUTPUT) {
-        PIN_MAP[pin].pin_mode = OUTPUT;
-        if(gpio_pin > 33){
-            //unlock gpio
-            return;//pins above 33 can be only inputs
-        } else if(gpio_pin < 32) {
-            GPIO.enable_w1ts = ((uint32_t)1 << gpio_pin);
-        } else {
-            GPIO.enable1_w1ts.val = ((uint32_t)1 << (gpio_pin - 32));
-        }
-    } else if(setMode & INPUT_PULLUP) {
-        PIN_MAP[pin].pin_mode = INPUT_PULLUP;
-        pinFunction |= FUN_PU;
-    } else if(setMode & INPUT_PULLDOWN) {
-        PIN_MAP[pin].pin_mode = INPUT_PULLDOWN;
-        pinFunction |= FUN_PD;
-    }
-
-    pinFunction |= ((uint32_t)2 << FUN_DRV_S);//what are the drivers?
-    pinFunction |= FUN_IE;//input enable but required for output as well?
-
-    if(setMode & (INPUT | OUTPUT)) {
-        pinFunction |= ((uint32_t)2 << MCU_SEL_S);
-    } else if(setMode == SPECIAL) {
-        pinFunction |= ((uint32_t)(((gpio_pin)==1||(gpio_pin)==3)?0:1) << MCU_SEL_S);
-    } else {
-        pinFunction |= ((uint32_t)(setMode >> 5) << MCU_SEL_S);
-    }
-
-    ESP_REG(DR_REG_IO_MUX_BASE + esp32_gpioMux[gpio_pin].reg) = pinFunction;
-
-    if(setMode & OPEN_DRAIN) {
-        pinControl = (1 << GPIO_PIN0_PAD_DRIVER_S);
-    }
-
-    GPIO.pin[gpio_pin].val = pinControl;
+    __pinMode(gpio_pin, mode);
 }
 
 /*
