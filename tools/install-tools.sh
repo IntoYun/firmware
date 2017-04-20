811fe8eb9df2942cc0531a70cf57e71c4289c59e
chenkaiyao <robin@molmc.com>
2017-04-20 23:25:51 +0800

    1. 更新tools 下载脚本 2. 更新release 子模块

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -31,7 +31,7 @@ sysType=`uname -s`
 cecho "Your system is $sysType " $blue
 if [ $sysType = "Linux" ]; then
     cecho "--->install arm gnu toolchain " $blue
-    wget -nc https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
+    wget https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
     tar -jxf gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
     mv gcc-arm-none-eabi-4_9-2015q3 ../gcc-arm-none-eabi
     cecho "--->install success " $blue
@@ -39,31 +39,31 @@ if [ $sysType = "Linux" ]; then
     if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then
         cecho "Your system is 64 bit " $blue
         cecho "--->install xtensa lx106 gnu toolchain " $blue
-        wget -nc http://arduino.esp8266.com/linux64-xtensa-lx106-elf-gb404fb9.tar.gz
+        wget http://arduino.esp8266.com/linux64-xtensa-lx106-elf-gb404fb9.tar.gz
         tar -zxf linux64-xtensa-lx106-elf-gb404fb9.tar.gz
         mv  xtensa-lx106-elf ../xtensa-lx106-elf
         cecho "--->install success " $blue
 
         cecho "--->install xtensa esp32 toolchain " $blue
-        wget -nc https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
+        wget https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
         tar -zxf xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz
         mv  xtensa-esp32-elf ../xtensa-esp32-elf
         cecho "--->install success " $blue
 
         cecho "--->install st-flash " $blue
-        wget -nc https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-linux64.tar.gz
+        wget https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-linux64.tar.gz
         tar -zxf stlink-1.2.0-linux64.tar.gz
         mv stlink-1.2.0-linux64 ../stlink
         cecho "--->install success " $blue
 
         cecho "--->install esp8266 esptool " $blue
-        wget -nc https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-linux64.tar.gz
+        wget https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-linux64.tar.gz
         tar -zxf esptool-0.4.11-linux64.tar.gz
         mv esptool-0.4.11-linux64 ../esp8266
         cecho "--->install success " $blue
 
         cecho "--->install esp32 esptool " $blue
-        wget -nc https://dl.espressif.com/dl/esptool-fe69994-linux64.tar.gz
+        wget https://dl.espressif.com/dl/esptool-fe69994-linux64.tar.gz
         tar -zxf esptool-fe69994-linux64.tar.gz
         mkdir ../esp32
         mv esptool ../esp32/
@@ -72,31 +72,31 @@ if [ $sysType = "Linux" ]; then
     else
         cecho "Your system is 32 bit " $blue
         cecho "--->install xtensa lx106 gnu toolchain " $blue
-        wget -nc http://arduino.esp8266.com/linux32-xtensa-lx106-elf.tar.gz
+        wget http://arduino.esp8266.com/linux32-xtensa-lx106-elf.tar.gz
         tar -zxf linux32-xtensa-lx106-elf.tar.gz
         mv  xtensa-lx106-elf ../xtensa-lx106-elf
         cecho "--->install success " $blue
 
         cecho "--->install xtensa esp32 toolchain " $blue
-        wget -nc https://dl.espressif.com/dl/xtensa-esp32-elf-linux32-1.22.0-61-gab8375a-5.2.0.tar.gz
+        wget https://dl.espressif.com/dl/xtensa-esp32-elf-linux32-1.22.0-61-gab8375a-5.2.0.tar.gz
         tar -zxf xtensa-esp32-elf-linux32-1.22.0-61-gab8375a-5.2.0.tar.gz
         mv  xtensa-esp32-elf ../xtensa-esp32-elf
         cecho "--->install success " $blue
 
         cecho "--->install st-flash " $blue
-        wget -nc https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-linux32.tar.gz
+        wget https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-linux32.tar.gz
         tar -zxf stlink-1.2.0-linux32.tar.gz
         mv stlink-1.2.0-linux32 ../stlink
         cecho "--->install success " $blue
 
         cecho "--->install esp8266 esptool " $blue
-        wget -nc https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-linux64.tar.gz
+        wget https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-linux64.tar.gz
         tar -zxf esptool-0.4.11-linux64.tar.gz
         mv  esptool-0.4.11-linux64 ../esp8266
         cecho "--->install success " $blue
 
         cecho "--->install esp32 esptool " $blue
-        wget -nc https://dl.espressif.com/dl/esptool-fe69994-linux32.tar.gz
+        wget https://dl.espressif.com/dl/esptool-fe69994-linux32.tar.gz
         tar -zxf esptool-fe69994-linux32.tar.gz
         mkdir ../esp32
         mv esptool ../esp32/
@@ -109,39 +109,39 @@ if [ $sysType = "Linux" ]; then
 
 elif [ $sysType = "Darwin" ]; then
     cecho "--->install arm gnu toolchain " $blue
-    wget -nc -c -t 10 https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2
+    wget https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2
     tar -jxf  gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2
     mv gcc-arm-none-eabi-4_9-2015q2 ../gcc-arm-none-eabi
     rm -rf gcc-arm-none-eabi-4_9-2015q2
     cecho "--->install success " $blue
 
     cecho "--->install xtensa lx106 gnu toolchain " $blue
-    wget -nc -c -t 10 http://arduino.esp8266.com/osx-xtensa-lx106-elf-gb404fb9-2.tar.gz
+    wget http://arduino.esp8266.com/osx-xtensa-lx106-elf-gb404fb9-2.tar.gz
     tar -zxf osx-xtensa-lx106-elf-gb404fb9-2.tar.gz
     mv xtensa-lx106-elf ../xtensa-lx106-elf
     cecho "--->install success " $blue
 
     cecho "--->install xtensa esp32 toolchain " $blue
-    wget -nc https://dl.espressif.com/dl/xtensa-esp32-elf-osx-1.22.0-61-gab8375a-5.2.0.tar.gz
+    wget https://dl.espressif.com/dl/xtensa-esp32-elf-osx-1.22.0-61-gab8375a-5.2.0.tar.gz
     tar -zxf xtensa-esp32-elf-osx-1.22.0-61-gab8375a-5.2.0.tar.gz
     mv  xtensa-esp32-elf ../xtensa-esp32-elf
     cecho "--->install success " $blue
 
     cecho "--->install st-flash " $blue
-    wget -nc -c -t 10 https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-osx.tar.gz
+    wget https://github.com/IntoRobot/stlink/releases/download/1.2.0/stlink-1.2.0-osx.tar.gz
     tar -zxf stlink-1.2.0-osx.tar.gz
     mv stlink-1.2.0-osx ../stlink
     cecho "--->install success " $blue
 
     cecho "--->install esp8266 esptool " $blue
-    wget -nc -c -t 10 https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-osx.tar.gz
+    wget https://github.com/igrr/esptool-ck/releases/download/0.4.11/esptool-0.4.11-osx.tar.gz
     tar -zxf esptool-0.4.11-osx.tar.gz
     mv esptool-0.4.11-osx ../esp8266
     cecho "--->install success " $blue
 
     cecho "--->install esp32 esptool " $blue
-    wget -nc https://dl.espressif.com/dl/esptool-fe69994-macos.tar.gz
-    tar -zxf esptool-fe69994-macos.tar.gz
+    wget https://dl.espressif.com/dl/esptool-9072736-macos.tar.gz
+    tar -zxf esptool-9072736-macos.tar.gz
     mkdir ../esp32
     mv esptool ../esp32/
     cecho "--->install success " $blue
